1b0b13a463a7b620a51500798b1418c6c7301406
allanchr4064 <lanchr4064@gmail.com>
2025-05-18 15:57:06 +0200

    ff

--- join-domain.ps1
+++ join-domain.ps1
@@ -5,12 +5,16 @@ param(
     [string]$dcIPAddress
 )
 
-# Find netværksinterface
 $interface = Get-NetAdapter | Where-Object { $_.Status -eq "Up" } | Select-Object -First 1
 
 # Sæt DNS til DC01
 Set-DnsClientServerAddress -InterfaceAlias $interface.Name -ServerAddresses $dcIPAddress
 
 # Join domænet
-$cred = New-Object System.Management.Automation.PSCredential("$domainName\$domainAdminUser", $domainAdminPassword)
-Add-Computer -DomainName $domainName -Credential $cred -Force -Restart
+try {
+    $cred = New-Object System.Management.Automation.PSCredential("$domainName\$domainAdminUser", $domainAdminPassword)
+    Add-Computer -DomainName $domainName -Credential $cred -Force -Restart
+} catch {
+    $_ | Out-File -FilePath C:\Temp\domain-join-error.txt -Encoding utf8
+    throw $_
+}
