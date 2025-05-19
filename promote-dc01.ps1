321513feff16fadba6e08e1cd6697a3c35217dd6
allanchr4064 <lanchr4064@gmail.com>
2025-05-19 11:13:43 +0200

    sfs

--- promote-dc01.ps1
+++ promote-dc01.ps1
@@ -14,7 +14,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 # Find aktivt netværkskort
 $interface = Get-NetAdapter | Where-Object { $_.Status -eq "Up" } | Select-Object -First 1
 if (-not $interface) {
-    Write-Error "❌ Intet aktivt netværkskort fundet"
+    Write-Error "❌ Intet aktivt netværkskort blev fundet"
     exit 1
 }
 
